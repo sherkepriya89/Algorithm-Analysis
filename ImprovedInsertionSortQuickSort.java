27f1fd94266a75eb9dd962a749a275c8117cfd9e
priyasherke <38167388+sherkepriya89@users.noreply.github.com>
2022-10-13 06:00:32 -0400

    Update ImprovedInsertionSortQuickSort.java

--- ImprovedInsertionSortQuickSort.java
+++ ImprovedInsertionSortQuickSort.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class insertionSortQuickSort {
+public class InsertionSortQuickSort {
 
 	// Main Function
 	public static void main(String[] args) {
